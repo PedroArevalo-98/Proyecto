195248a9eb9e16816cf626e954179e655292ed60
almuflores <43790828+almuflores@users.noreply.github.com>
2020-02-07 09:23:04 +0100

    Update Prestamo.java

--- Proyecto/proyecto/src/proyecto/Prestamo.java
+++ Proyecto/proyecto/src/proyecto/Prestamo.java
@@ -4,13 +4,30 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
+/**
+ * Definimos la clase Prestamo  
+ * @author almudenaflores
+ * @version 6/02/2020
+ */
+
+
 public class Prestamo {
+	/**
+	 * definimos las variables
+	 */
 	private LocalDateTime fechaSalida;
     private LocalDateTime fechaDevolucion;
     private double idUsuario; 
     private double codigoLibro;
     
     private ArrayList<String> codigosLibrosPrestados; 
+	
+	/**
+     * Constructor de la clase
+     * @param d
+     * @param codigoLibro
+     * @param fecha
+     */
     
     public Prestamo(double d, double codigoLibro, LocalDateTime fecha){
         this.idUsuario = d;
@@ -19,6 +36,10 @@ public class Prestamo {
         this.fechaDevolucion = fecha.plus(30, ChronoUnit.DAYS);
         this.codigosLibrosPrestados = new ArrayList();
     }
+	/**
+     * getters y setters
+     * @return devuelve el atributo
+     */
 
     public LocalDateTime getFechaSalida() {
         return fechaSalida;
@@ -49,11 +70,11 @@ public class Prestamo {
         codigosLibrosPrestados.add(codigoLibro);
     }
     
-    public void devolverLibro(){
-    }
-    
-    public void multa(){
-    }
+    /**
+     * Metodo para buscar un prestamo hecho
+     * @param codigoLibro2
+     * @return true si el prestamo existe, false si no
+     */
     public boolean buscarPrestamo (double codigoLibro2) {
 		
 			if (codigoLibro==codigoLibro2) {
@@ -78,6 +99,11 @@ public class Prestamo {
 	public void setCodigosLibrosPrestados(ArrayList<String> codigosLibrosPrestados) {
 		this.codigosLibrosPrestados = codigosLibrosPrestados;
 	}
+	
+	/**
+	 * Metodo para mostrar los prestamos hechos hasta ese momento con la fecha del prestamo, la fecha de devolucion, el carnet del socio que tiene
+	 * el libro y el codigo del libro
+	 */
 	public void mostrarPrestamo() {
 		System.out.println(" fecha de prestamo "+fechaSalida +" fecha de devolucion " +fechaDevolucion +" carnet de socio poseedor del libro " +idUsuario +" codigo del libro "+codigoLibro);
 	}
